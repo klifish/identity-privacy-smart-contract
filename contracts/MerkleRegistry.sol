55e9560651c84e7c09502df81a366c6e2dd75ac7
Kun Li <likun6034@gmail.com>
2025-01-18 12:34:00 +0100

    solidity hash left right done

--- contracts/MerkleRegistry.sol
+++ contracts/MerkleRegistry.sol
@@ -8,13 +8,13 @@ contract MerkleRegistry is MerkleTreeWithHistory {
     mapping(bytes32 => bool) public usedNullifiers; // Track used nullifiers to prevent proof reuse
 
     // Events
-    event UserRegistered(bytes32 leaf, uint32 index);
+    event UserRegistered(uint256 leaf, uint32 index);
     // event ProofVerified(bytes32 nullifier);
 
     constructor(uint32 _levels, IHasher _hasher) MerkleTreeWithHistory(_levels, _hasher) {}
 
     // Function to register a new user by inserting a new leaf into the Merkle tree
-    function registerUser(bytes32 _leaf) external {
+    function registerUser(uint256 _leaf) external {
         // console.log("Registering leaf:", uint256(_leaf));
 
         uint32 index = _insert(_leaf);
@@ -30,41 +30,41 @@ contract MerkleRegistry is MerkleTreeWithHistory {
         levels = _levels;
     }
     // Function to verify a proof that a user is registered
-    // function verifyProof(
-    //     bytes32 nullifier, // Nullifier to ensure one-time proof usage
-    //     bytes32 leaf, // The user's hashed identifier (leaf in the Merkle tree)
-    //     bytes32[] memory proof, // Merkle proof showing the user's identifier is part of the tree
-    //     uint256[] memory positions // Indicates left (0) or right (1) sibling nodes in each proof level
-    // ) external {
-    //     require(!usedNullifiers[nullifier], "Nullifier has already been used");
+    function verifyProof(
+        bytes32 nullifier, // Nullifier to ensure one-time proof usage
+        bytes32 leaf, // The user's hashed identifier (leaf in the Merkle tree)
+        bytes32[] memory proof, // Merkle proof showing the user's identifier is part of the tree
+        uint256[] memory positions // Indicates left (0) or right (1) sibling nodes in each proof level
+    ) external {
+        require(!usedNullifiers[nullifier], "Nullifier has already been used");
 
-    //     // Verify the Merkle proof
-    //     require(verifyMerkleProof(leaf, proof, positions), "Invalid Merkle proof");
+        // Verify the Merkle proof
+        require(verifyMerkleProof(leaf, proof, positions), "Invalid Merkle proof");
 
-    //     // Mark the nullifier as used
-    //     usedNullifiers[nullifier] = true;
+        // Mark the nullifier as used
+        usedNullifiers[nullifier] = true;
 
-    //     // Emit an event indicating the proof was successfully verified
-    //     emit ProofVerified(nullifier);
-    // }
+        // Emit an event indicating the proof was successfully verified
+        emit ProofVerified(nullifier);
+    }
 
-    // // Function to verify the Merkle proof on-chain
-    // function verifyMerkleProof(
-    //     bytes32 leaf,
-    //     bytes32[] memory proof,
-    //     uint256[] memory positions
-    // ) public view returns (bool) {
-    //     bytes32 computedHash = leaf;
+    // Function to verify the Merkle proof on-chain
+    function verifyMerkleProof(
+        bytes32 leaf,
+        bytes32[] memory proof,
+        uint256[] memory positions
+    ) public view returns (bool) {
+        bytes32 computedHash = leaf;
 
-    //     for (uint256 i = 0; i < proof.length; i++) {
-    //         if (positions[i] == 0) {
-    //             computedHash = hashLeftRight(hasher, computedHash, proof[i]);
-    //         } else {
-    //             computedHash = hashLeftRight(hasher, proof[i], computedHash);
-    //         }
-    //     }
+        for (uint256 i = 0; i < proof.length; i++) {
+            if (positions[i] == 0) {
+                computedHash = hashLeftRight(hasher, computedHash, proof[i]);
+            } else {
+                computedHash = hashLeftRight(hasher, proof[i], computedHash);
+            }
+        }
 
-    //     // Check if the computed hash matches the current Merkle root
-    //     return isKnownRoot(computedHash);
-    // }
+        // Check if the computed hash matches the current Merkle root
+        return isKnownRoot(computedHash);
+    }
 }
--- test/MerkleRegistry.js
+++ test/MerkleRegistry.js
@@ -7,6 +7,7 @@ const ffjavascript = require("ffjavascript");
 const merkleTree = require('fixed-merkle-tree');
 const { setupHasher, hashLeftRight } = require('../scripts/utilities/hasher');
 const exp = require("constants");
+const utils = require('../scripts/utils');
 
 const SEED = "mimcsponge";
 
@@ -19,6 +20,7 @@ describe('Registry Smart Contract', function () {
 
     let pedersen;
     let babyjub;
+    let F
 
     let hasher;
     let RegisterVerifierContract;
@@ -36,6 +38,7 @@ describe('Registry Smart Contract', function () {
         RegistryContract = await ethers.getContractFactory("MerkleRegistry")
         pedersen = await circomlibjs.buildPedersenHash();
         babyjub = await circomlibjs.buildBabyjub();
+        F = babyjub.F;
     });
 
     async function fixture() {
@@ -52,7 +55,7 @@ describe('Registry Smart Contract', function () {
         const hasherContract = await C.deploy();
         const hasherAddress = await hasherContract.getAddress();
 
-        
+
 
         const registry = await RegistryContract.deploy(MERKLE_TREE_LEVEL, hasherAddress);
         return { signers, signer, signer1, registry, pedersen, babyjub, hasherContract };
@@ -87,7 +90,7 @@ describe('Registry Smart Contract', function () {
     it('Should deploy the Registry smart contract', async function () {
         const hasherOnChainAddress = await hasherOnChain.getAddress();
         registry = await RegistryContract.deploy(MERKLE_TREE_LEVEL, hasherOnChainAddress);
-        
+
         const registryAddress = await registry.getAddress();
         expect(registryAddress).to.be.properAddress;
 
@@ -105,39 +108,56 @@ describe('Registry Smart Contract', function () {
     });
 
     it('Should calculate hashLeftRight correctly', async function () {
-        // const { signers, signer, signer1, registry, pedersen, babyjub, hasherContract } = await loadFixture(fixture);
-        const signers = await ethers.getSigners();
-        const signer = signers[0];
-        const hash = pedersen.hash(await signer.getAddress());
-        const point = babyjub.unpackPoint(hash);
-        const point_x = ffjavascript.utils.leBuff2int(point[0]);
-        const point_x_hex = '0x' + BigInt(point_x).toString(16);
-        const leaf = point_x_hex;
-        const hashLeftRightValue = hashLeftRight(hasher, leaf, 0);
 
-        const hashLeftRightValueHex = BigInt(hashLeftRightValue).toString(16);
-        const hashLeftRightValueHex256 = '0x' + hashLeftRightValueHex.padStart(64, '0');
+        const signers = await ethers.getSigners();
+        const signer0 = signers[0];
+        address0 = await signer0.getAddress();
+
+        const secret = "secret"
+        const secretBuff = (new TextEncoder()).encode(secret)
+        const secretBigInt = ffjavascript.utils.leBuff2int(secretBuff)
+
+        const nullifier = 1n;
+
+        const src = [address0, secretBigInt, nullifier]
+        leaf = await utils.pedersenHashMultipleInputs(src)
+
+        const hP = babyjub.unpackPoint(leaf);
+        hp1 = F.toObject(hP[1])
+
+        const hashLeftRightValue = hashLeftRight(hasher, hp1, 0);
+
 
         const hasherContractaddress = await hasherOnChain.getAddress();
 
         const zero = BigInt(0).toString(16);
         const zero_256 = '0x' + zero.padStart(64, '0');
 
-        const hashLeftRightOnChain = await registry.hashLeftRight(hasherContractaddress, leaf, zero_256);
+        const hp1_hex = '0x'+BigInt(hp1).toString(16);
+        const hashLeftRightOnChain = await registry.hashLeftRight(hasherContractaddress, hp1_hex, zero_256);
 
-        expect(hashLeftRightOnChain).to.equal(hashLeftRightValueHex256);
+        expect(hashLeftRightOnChain).to.equal(BigInt(hashLeftRightValue));
     })
 
     it('Should register a user', async function () {
-        const signers = await ethers.getSigners();
-        const signer = signers[0];
 
-        const hash = pedersen.hash(await signer.getAddress());
-        const point = babyjub.unpackPoint(hash);
-        const point_x = ffjavascript.utils.leBuff2int(point[0]);
-        const point_x_hex = '0x' + BigInt(point_x).toString(16);
-        const leaf = point_x_hex;
-        await expect(registry.registerUser(leaf)).to.emit(registry, 'UserRegistered').withArgs(leaf, 0);
+        const signers = await ethers.getSigners();
+        const signer0 = signers[0];
+        address0 = await signer0.getAddress();
+
+        const secret = "secret"
+        const secretBuff = (new TextEncoder()).encode(secret)
+        const secretBigInt = ffjavascript.utils.leBuff2int(secretBuff)
+
+        const nullifier = 1n;
+
+        const src = [address0, secretBigInt, nullifier]
+        leaf = await utils.pedersenHashMultipleInputs(src)
+
+        const hP = babyjub.unpackPoint(leaf);
+        hp1 = F.toObject(hP[1])
+
+        await expect(registry.registerUser(hp1)).to.emit(registry, 'UserRegistered').withArgs(hp1, 0);
 
     })
 
@@ -178,41 +198,41 @@ describe('Registry Smart Contract', function () {
         expect(zero_tree.root).to.equal(hashLeftRight(hasher, leaf, 0));
     })
 
-    it('Should construct a one level Merkle Tree', async function () {
-        let point, point_x, point_x_hex, leaf;
+    // it('Should construct a one level Merkle Tree', async function () {
+    //     let point, point_x, point_x_hex, leaf;
 
-        // const { signers, signer, signer1, registry, pedersen, babyjub } = await loadFixture(fixture);
+    //     // const { signers, signer, signer1, registry, pedersen, babyjub } = await loadFixture(fixture);
 
-        const signers = await ethers.getSigners();
+    //     const signers = await ethers.getSigners();
 
-        const last_root = await registry.getLastRoot();
+    //     const last_root = await registry.getLastRoot();
 
-        const events = await registry.queryFilter('UserRegistered');
+    //     const events = await registry.queryFilter('UserRegistered');
 
-        const sortedEvents = events.sort((a, b) => (a.args.index < b.args.index ? -1 : a.args.index > b.args.index ? 1 : 0));
+    //     const sortedEvents = events.sort((a, b) => (a.args.index < b.args.index ? -1 : a.args.index > b.args.index ? 1 : 0));
 
-        const leafs = sortedEvents.map(event => event.args.leaf);
+    //     const leafs = sortedEvents.map(event => event.args.leaf);
 
-        const tree = new merkleTree.MerkleTree(MERKLE_TREE_LEVEL, leafs, { hashFunction: (left, right) => hashLeftRight(hasher, left, right) });
+    //     const tree = new merkleTree.MerkleTree(MERKLE_TREE_LEVEL, leafs, { hashFunction: (left, right) => hashLeftRight(hasher, left, right) });
 
-        const hash = pedersen.hash(await signers[0].getAddress());
-        point = babyjub.unpackPoint(hash);
-        point_x = ffjavascript.utils.leBuff2int(point[0]);
-        hex_string = BigInt(point_x).toString(16);
-        point_x_hex = '0x' + hex_string.padStart(64, '0');
-        user_0 = point_x_hex;
+    //     const hash = pedersen.hash(await signers[0].getAddress());
+    //     point = babyjub.unpackPoint(hash);
+    //     point_x = ffjavascript.utils.leBuff2int(point[0]);
+    //     hex_string = BigInt(point_x).toString(16);
+    //     point_x_hex = '0x' + hex_string.padStart(64, '0');
+    //     user_0 = point_x_hex;
 
-        const registerEvent = events.find(event => event.args.leaf === user_0);
+    //     const registerEvent = events.find(event => event.args.leaf === user_0);
 
-        const merkleProof = tree.proof(user_0);
-        console.log(merkleProof);
+    //     const merkleProof = tree.proof(user_0);
+    //     // console.log(merkleProof);
 
-        const root = tree.proof(user_0).pathRoot;
-        hex_root = BigInt(root).toString(16);
-        hex_root_256 = '0x' + hex_root.padStart(64, '0');
-        console.log(hex_root_256);
+    //     const root = tree.proof(user_0).pathRoot;
+    //     hex_root = BigInt(root).toString(16);
+    //     hex_root_256 = '0x' + hex_root.padStart(64, '0');
+    //     console.log(hex_root_256);
 
-        const isValidRoot = await registry.isKnownRoot(hex_root_256);
-        expect(isValidRoot).to.be.true;
-    })
+    //     const isValidRoot = await registry.isKnownRoot(hex_root_256);
+    //     expect(isValidRoot).to.be.true;
+    // })
 })
